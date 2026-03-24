407a7c34ae0c8539cf9f1381ac08a34a522ed1be
Tunahan Özdil <tunahanozdil2023@gmail.com>
2026-03-24 12:03:04 +0300

    feat(ai-service): implement Repository Pattern and database integration (SCRUM-12)

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,13 +1,23 @@
 import os
 import json
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from google import genai
+from sqlalchemy.orm import Session
+
+# --- YENİ: VERİTABANI İÇİN GEREKLİ DOSYALARI İÇE AKTARIYORUZ ---
+from database import engine, get_db
+import models
+from repository import AnalysisRepository
+# --------------------------------------------------------------
 
 # .env yükle
 load_dotenv()
+
+# YENİ: Veritabanında tabloları otomatik oluştur (Eğer yoksa)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="ResiliEngine - AI Analysis Service")
 
@@ -21,13 +31,12 @@ if not api_key:
 client = genai.Client(api_key=api_key)
 
 
-
 class LogRequest(BaseModel):
     log_content: str
 
-
+# YENİ: Fonksiyona "db: Session = Depends(get_db)" ekledik ki veritabanına bağlanabilelim
 @app.post("/analyze")
-async def analyze_logs(request: LogRequest):
+async def analyze_logs(request: LogRequest, db: Session = Depends(get_db)):
 
     prompt = f"""
 Sen bir Yazılım Mimarı ve Kaos Mühendisliği uzmanısın.
@@ -46,7 +55,6 @@ Yanıtı SADECE şu JSON formatında ver:
 """
 
     try:
-        # HATA DÜZELTME: 'gemini-1.5-flash-latest' yerine 'gemini-1.5-flash' kullan
         response = client.models.generate_content(
             model="models/gemini-flash-latest", 
             contents=prompt
@@ -54,7 +62,19 @@ Yanıtı SADECE şu JSON formatında ver:
 
         text = response.text
 
-        result = json.loads(text)
+        # HATA ÖNLEME: Gemini cevabın başına/sonuna ```json koyarsa kod çökmesin diye temizliyoruz
+        clean_text = text.replace("```json", "").replace("```", "").strip()
+
+        result = json.loads(clean_text)
+
+        # --- YENİ GÖREV (SCRUM-10): ANALİZİ VERİTABANINA KAYDET ---
+        repo = AnalysisRepository(db)
+        repo.save_analysis(
+            log_content=request.log_content,
+            analysis=result["analysis"],
+            recommendation=result["recommendation"]
+        )
+        # ----------------------------------------------------------
 
         return result
 
@@ -64,7 +84,7 @@ Yanıtı SADECE şu JSON formatında ver:
 
         raise HTTPException(
             status_code=500,
-            detail=f"Gemini hata döndürdü: {str(e)}"
+            detail=f"Gemini veya Veritabanı hata döndürdü: {str(e)}"
         )
 
 
