f30c212a5a4e5ed3a9703eb48402aebf4b4079f2
ardaysuf <yusufarda201514@gmail.com>
2026-04-03 12:19:59 +0300

    feat: complete backend unit tests and enable CORS for stage 5 dashboard

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // STAGE 5 İÇİN Taha'nın beklediği CORS ayarı
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
