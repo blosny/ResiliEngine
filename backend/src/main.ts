99de7d04d970783505595a6a4d39a921aaccdaca
Taha Buğra Çiçek <tahabugracck@gmail.com>
2026-04-20 19:00:02 +0300

    HOTFIX: CORS, TypeScript promise hataları ve biçimlendirme sorunları giderildi

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS Fix
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+// 'Promises must be awaited' hatası için 'void' ekliyoruz
+void bootstrap();
